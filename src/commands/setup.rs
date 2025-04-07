41d7cb3585cabee730d8088e221aa952ad2e59b5
Efe Karasakal <dogukankrskl@gmail.com>
2025-04-07 19:04:58 +0200

    feat: check if setup exists

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -1,7 +1,21 @@
 use crate::config::save_config;
+use crate::config::{CONFIG_PATH, config_exists};
 use dialoguer::{Confirm, Input};
 
 pub fn setup() {
+    if config_exists() {
+        let confirm = Confirm::new()
+            .with_prompt("⚠️ Config already exists. Are you sure you want to overwrite it?")
+            .default(false)
+            .interact()
+            .unwrap();
+
+        if !confirm {
+            println!("❌ Aborted");
+            return;
+        }
+    }
+
     let token: String = Input::new()
         .with_prompt("🔑 Notion Integration Token")
         .interact_text()
@@ -13,7 +27,7 @@ pub fn setup() {
         .unwrap();
 
     let confirm = Confirm::new()
-        .with_prompt("Save to ~/.gj/config.json ?")
+        .with_prompt(format!("Save to ~/{CONFIG_PATH} ?"))
         .default(true)
         .interact()
         .unwrap();
--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,11 @@ use serde_json::json;
 use std::fs;
 use std::path::PathBuf;
 
-fn get_config_path() -> PathBuf {
-    let mut path = home_dir().expect("No home dir found");
-    path.push(".gj/config.json");
-    path
+pub const CONFIG_PATH: &str = ".gj/config.json";
+
+pub fn config_exists() -> bool {
+    let path = get_config_path();
+    path.exists()
 }
 
 pub fn load_config() -> Config {
@@ -25,5 +26,11 @@ pub fn save_config(token: String, db: String) {
     let parent = path.parent().unwrap();
     fs::create_dir_all(parent).unwrap();
     fs::write(path, serde_json::to_string_pretty(&config).unwrap()).unwrap();
-    println!("✅ Config saved to ~/.gj/config.json");
+    println!("✅ Config saved to ~/{CONFIG_PATH}");
+}
+
+fn get_config_path() -> PathBuf {
+    let mut path = home_dir().expect("No home dir found");
+    path.push(CONFIG_PATH);
+    path
 }
